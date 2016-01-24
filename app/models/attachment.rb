4aaeeb48295feb783c0e97079fb99766d6457c4e
sandralouise <sandra.adamshallie@gmail.com>
2016-01-24 19:43:38 +0000

    Create attachment option

---
+++ app/models/attachment.rb
@@ -0,0 +1,17 @@
+class Attachment < ActiveRecord::Base
+  attr_accessor :upload
+  belongs_to :project
+  
+  MAX_FILESIZE = 10.megabytes
+  validates_presence_of :name, :upload
+  validates_uniqueness_of :name
+  
+  validate :uploaded_file_size
+  
+  private
+    if upload
+      error.add(:upload, "File size must be less than #{self.class::MAX_FILESIZE}") 
+      unless upload.size <= self.class::MAX_FILESIZE
+    end
+  end
+end
